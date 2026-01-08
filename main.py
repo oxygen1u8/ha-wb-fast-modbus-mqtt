58f52e06bcf248aa4f4b0c7595396ef4f39953dc
Egor Smolentsev <egors5779@gmail.com>
2026-01-08 21:03:29 +0300

    main: update main algorithm

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastmodbus import FastModbusSerialClient
+import json
+import sys
 
-client = FastModbusSerialClient("/dev/ttyACM0", baudrate=115200)
-client.connect()
-client.scan()
-print(client.slave_map)
-client.close()
+path_to_options = sys.argv[1]
+
+with open(path_to_options, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+ports = []
+for port in data["Serial port config"]:
+    port_path = port["Port"]
+    baudrate = int(port["Baudrate"])
+    stop_bit_count = int(port["Stop bit count"])
+    parity = port["Parity"]
+    ports.append(
+        FastModbusSerialClient(
+            port=port_path, baudrate=baudrate, stopbits=stop_bit_count, parity=parity
+        )
+    )
+
+for port in ports:
+    port.connect()
+    port.scan()
+    print(port.slave_map)
+
+for port in ports:
+    port.close()
