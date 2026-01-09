50ab137e49844e91870a6c81e71fedb46b190d40
Egor Smolentsev <egors5779@gmail.com>
2026-01-09 09:11:38 +0300

    main: add args processing via argparse

--- ha-wb-fast-modbus-mqtt/main.py
+++ ha-wb-fast-modbus-mqtt/main.py
@@ -1,28 +1,39 @@
 from fastmodbus import FastModbusSerialClient
 import json
-import sys
+import argparse
 
-path_to_options = sys.argv[1]
+def main():
+    parser = argparse.ArgumentParser(description='Fast Modbus')
+    parser.add_argument('--options', type=str, help='Путь до options.json')
 
-with open(path_to_options, "r", encoding="utf-8") as f:
-    data = json.load(f)
+    args = parser.parse_args()
+    path_to_options = args.options
 
-ports = []
-for port in data["Serial port config"]:
-    port_path = port["Port"]
-    baudrate = int(port["Baudrate"])
-    stop_bit_count = int(port["Stop bit count"])
-    parity = port["Parity"]
-    ports.append(
-        FastModbusSerialClient(
-            port=port_path, baudrate=baudrate, stopbits=stop_bit_count, parity=parity
+    with open(path_to_options, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    ports = []
+    for port in data["Serial port config"]:
+        port_path = port["Port"]
+        baudrate = int(port["Baudrate"])
+        stop_bit_count = int(port["Stop bit count"])
+        parity = port["Parity"]
+        ports.append(
+            FastModbusSerialClient(
+                port=port_path,
+                baudrate=baudrate,
+                stopbits=stop_bit_count,
+                parity=parity
+            )
         )
-    )
 
-for port in ports:
-    port.connect()
-    port.scan()
-    print(port.slave_map)
+    for port in ports:
+        port.connect()
+        port.scan()
+        print(port.slave_map)
 
-for port in ports:
-    port.close()
+    for port in ports:
+        port.close()
+
+if __name__ == "__main__":
+    main()
