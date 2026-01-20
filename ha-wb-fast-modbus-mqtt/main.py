0f793d40095274854489050cf39964fa8b221d61
Egor Smolentsev <egors5779@gmail.com>
2026-01-20 22:28:09 +0300

    main: fix prints

--- ha-wb-fast-modbus-mqtt/main.py
+++ ha-wb-fast-modbus-mqtt/main.py
@@ -12,7 +12,8 @@ async def execute_scan(manager: FastModbusManager):
     t = time.time() - t
     print(f"[{manager.port}]: scan took {t} s")
     if len(slave_map):
-        print("\n".join(f"[{manager.port}]: {slave}" for slave in slave_map))
+        for slave in slave_map:
+            print(f"[{manager.port}]: {slave}")
     else:
         print(f"[{manager.port}]: no Fast Modbus device found")
 
