81e8037cf3d8eca14460aaca86a245bb05aa03bb
Egor Smolentsev <egors5779@gmail.com>
2026-01-20 22:22:03 +0300

    main: update main algorithm

--- ha-wb-fast-modbus-mqtt/main.py
+++ ha-wb-fast-modbus-mqtt/main.py
@@ -1,37 +1,20 @@
-from fastmodbus.asyncfastmodbus import AsyncFastModbusSerialClient
+from fastmodbus.manager import FastModbusManager
 from pymodbus.pdu.decoders import *
-import logging
 import asyncio
 import time
 import json
 import argparse
 
 
-async def scan(serial_port: str):
-    baudrates = [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
-    parity = ["O", "E", "N"]
-
-    print("Start Fast Modbus scan...")
-    slave_map = []
-    t1 = time.time()
-    for b in baudrates:
-        for p in parity:
-            client = AsyncFastModbusSerialClient(
-                serial_port, baudrate=b, parity=p
-            )
-            await client.connect()
-            print(f"[{serial_port}]: scan on {b} bps | parity: {p}")
-            result = await client.scan()
-            if len(result):
-                slave_map += result
-            client.close()
-    t2 = time.time()
-
-    print(f"[{serial_port}]: scan took {t2 - t1} s")
-    for slave in slave_map:
-        print(slave)
-
-    return slave_map
+async def execute_scan(manager: FastModbusManager):
+    t = time.time()
+    slave_map = await manager.scan_bus()
+    t = time.time() - t
+    print(f"[{manager.port}]: scan took {t} s")
+    if len(slave_map):
+        print("\n".join(f"[{manager.port}]: {slave}" for slave in slave_map))
+    else:
+        print(f"[{manager.port}]: no Fast Modbus device found")
 
 
 async def main():
@@ -44,8 +27,11 @@ async def main():
     with open(path_to_options, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    tasks = [asyncio.create_task(scan(port["Port"])) for port in data["Serial port config"]]
-    await asyncio.gather(*tasks)
+    port_managers = [FastModbusManager(port) for port in data["Serial port config"]]
+
+    await asyncio.gather(
+        *[asyncio.create_task(execute_scan(manager)) for manager in port_managers]
+    )
 
 
 if __name__ == "__main__":
