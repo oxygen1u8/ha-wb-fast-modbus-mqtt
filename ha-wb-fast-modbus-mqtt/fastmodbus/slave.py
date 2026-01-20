a37810920c3c8aeeb14e31916372842d306be4e9
Egor Smolentsev <egors5779@gmail.com>
2026-01-20 22:21:31 +0300

    fastmodbus_slave: update class structure

--- ha-wb-fast-modbus-mqtt/fastmodbus/slave.py
+++ ha-wb-fast-modbus-mqtt/fastmodbus/slave.py
@@ -5,47 +5,43 @@
 устройстве протокола Fast Modbus.
 """
 
+
 class FastModbusSlave:
     """
     Класс для представления устройства Fast Modbus.
-    
+
     Хранит информацию об уникальном идентификаторе устройства, серийном номере,
     параметрах соединения и предоставляет методы для его представления.
     """
-    
-    def __init__(
-        self, slave_id: int, serial_num: int, baudrate: int, parity: str, stop: int
-    ):
+
+    def __init__(self, slave_id: int, serial_num: int, baudrate: int, parity: str):
         """
         Инициализирует объект FastModbusSlave.
-        
+
         Args:
             slave_id (int): Идентификатор устройства
             serial_num (int): Серийный номер устройства
             baudrate (int): Скорость передачи данных
             parity (str): Тип проверки четности
-            stop (int): Количество стоп-битов
         """
         self.slave_id = slave_id
         self.serial_num = serial_num
         self.baudrate = baudrate
         self.parity = parity
-        self.stop = stop
-
 
     def __str__(self):
         """
         Возвращает строковое представление устройства.
-        
+
         Returns:
             str: Строковое представление устройства
         """
-        return f"Slave ID: {hex(self.slave_id)}, Serial: {hex(self.serial_num)}, Baudrate: {self.baudrate}, Parity: {self.parity}, Stop bits: {self.stop}"
-        
+        return f"Slave ID: {hex(self.slave_id)}, Serial: {hex(self.serial_num)}, Baudrate: {self.baudrate}, Parity: {self.parity}"
+
     def to_dict(self):
         """
         Преобразует объект в словарь.
-        
+
         Returns:
             dict: Словарь с параметрами устройства
         """
@@ -54,5 +50,4 @@ class FastModbusSlave:
             "serial_num": self.serial_num,
             "baudrate": self.baudrate,
             "parity": self.parity,
-            "stop_bits": self.stop
         }
