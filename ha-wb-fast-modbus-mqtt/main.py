63f44cc2e5a6d35feb4998f11c6dff1d8da31158
Egor Smolentsev <egors5779@gmail.com>
2026-01-21 23:29:10 +0300

    wiu wiu

--- ha-wb-fast-modbus-mqtt/main.py
+++ ha-wb-fast-modbus-mqtt/main.py
@@ -125,6 +125,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     path_to_options = args.options
+
+    logger.info("Open file options.json")
+    with open(path_to_options, "r", encoding="utf-8") as f:
+        logger.info(f.read())
     
     # Получаем порт из переменной окружения, если она установлена (для Home Assistant ingress)
     port = int(os.environ.get("PORT", 8000))
