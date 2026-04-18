a20468ba0a06c3ee2e451ed80925fd6998c5d365
Egor Smolencev <oxygen1u8@mail.ru>
2026-04-18 10:02:58 +0300

    webui: update DeviceModel content

--- ha-wb-fast-modbus-mqtt/webui/app/models/device.py
+++ ha-wb-fast-modbus-mqtt/webui/app/models/device.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from sqlalchemy import String, Boolean, Integer, Numeric
+from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
@@ -12,3 +12,6 @@ class Device(Base):
     serial_num: Mapped[int] = mapped_column(Integer)
     baudrate: Mapped[int] = mapped_column(Integer)
     parity: Mapped[str] = mapped_column(String(1))
+    bus_id: Mapped[int] = mapped_column(ForeignKey("bus.id"), nullable=False)
+
+    bus: Mapped["Bus"] = relationship("Bus", back_populates="devices")
