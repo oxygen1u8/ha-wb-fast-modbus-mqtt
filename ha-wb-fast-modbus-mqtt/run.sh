dd8c0992e9a53b12a686dece1a4a96a2c39fcd84
Egor Smolencev <oxygen1u8@mail.ru>
2026-04-20 21:36:01 +0300

    work-env: refactor addon content

--- ha-wb-fast-modbus-mqtt/run.sh
+++ ha-wb-fast-modbus-mqtt/run.sh
@@ -4,5 +4,7 @@ set -e
 
 source .venv/bin/activate
 
-echo "Start main.py"
-python3 main.py --options /data/options.json
+echo "Start uvicorn"
+export PATH_TO_OPTIONS="/data/options.json"
+cd webui
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8080
